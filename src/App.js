3ea03e00a60c5ac6049642e66e37fc741de7624d
sumedha-2323 <sumedhajammula@gmail.com>
2024-02-14 19:34:08 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import data from './data';
+// console.log(data);
+
+const App=()=>{
+  return(
+    <header className="container">
+      <ul className="comments-container">
+        {
+          data.map((eachComment)=>{
+            const {id, email, name, body}= eachComment;
+            return(
+            <li key={id} className="comment">
+              <div className="comments-header">
+            <h3 className="email">{email}</h3>
+            <h3 className="name">{name}</h3>
+          </div>
+          <p className="message">{body}</p> 
+            </li>
+            )
+
+          })
+        }
+        {/* <li className="comment">
+          <div className="comments-header">
+            <h3 className="email">email@gmail.com</h3>
+            <h3 className="name">some random name</h3>
+          </div>
+          <p className="message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste eos neque omnis dolorum suscipit rem quibusdam temporibus aspernatur voluptatum aut.</p> 
+        </li>
+        
+         */}
+      </ul>
+    </header>
+  )
 }
 
 export default App;
